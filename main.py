786fc52e03f9a1bd3765cd59df65372b7d44b49a
PabloFuente14 <pablofuente14@opendeusto.es>
2023-09-21 10:18:20 +0200

    Files Added

---
+++ main.py
@@ -0,0 +1,14 @@
+import extract_data
+import cross_data
+
+if __name__== '__main__':
+    automator = extract_data.Automator()
+    automator.run()
+    #data_valor_hora = automator.data_valor_hora
+    #data_htas = automator.data_htas
+    #data_ofs = automator.data_ofs
+
+    #------------------Nuevo
+    df_valor_hora_hta = cross_data.cruce_valor_y_hta(automator.data_valor_hora, automator.data_htas)
+
+    print("Hola...")
